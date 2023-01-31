f939879b7856c35ca44dce344f5758f47fdcda28
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-01-31 10:08:45 +0200

    Update تنظيف.py

--- sbb_b/plugins/تنظيف.py
+++ sbb_b/plugins/تنظيف.py
@@ -1,1 +1,7 @@
-.
+from telethon import functions
+from telethon.tl import functions
+from telethon.tl.functions.channels import InviteToChannelRequest
+
+from sbb_b import sbb_b
+
+from ..core.managers import edit_delete, edit_or_reply
